f955c6de58bc84ba19fb0e3a77316265010a810a
程剑辉 <15851476582@zrgj.com>
2022-06-20 01:48:33 +0800

    cjh

--- Postman_miniprogram/miniprogram/pages/login/login.js
+++ Postman_miniprogram/miniprogram/pages/login/login.js
@@ -1,66 +1,26 @@
 // pages/login/login.js
+import Toast from '../../miniprogram_npm/@vant/weapp/toast/toast'; 
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    username:null,
+    password:null
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+  login:function(){
+    if (this.data.username !== null && this.data.password !== null){
+      Toast.success('登录成功!')
+      setTimeout(function(){
+        wx.switchTab({
+          url: '/pages/home/home',
+        })
+      },1000)
+    }else{
+      Toast.fail("密码错误！")
+    }
   }
 })
