4def00fbcddf56d2ceb493654182204f3a277113
程剑辉 <15851476582@zrgj.com>
2022-06-21 01:06:06 +0800

    程剑辉

--- Postman_miniprogram/miniprogram/pages/editUserInfo/editUserInfo.js
+++ Postman_miniprogram/miniprogram/pages/editUserInfo/editUserInfo.js
@@ -37,16 +37,20 @@ Page({
     })
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
-  },
-
+  /*
+  * 监听picker变化
+  * */
   onChange(event) {
     const { picker, value, index } = event.detail;
   },
+
+  /**
+   * 获取用户手机号
+   * */
+  getPhoneNumber(e){
+    console.log(e.detail.code)
+  },
+
 
   /**
    * 修改用户信息*/
@@ -82,13 +86,6 @@ Page({
   printInput:function (e){},
 
   /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
    * 生命周期函数--监听页面显示
    */
   onShow() {
@@ -101,38 +98,4 @@ Page({
     })
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
 })
